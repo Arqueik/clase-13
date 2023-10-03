2c6563a393a107fef32f86610d756691b1c3500d
Alan Quiroga <arqueikk@gmail.com>
2023-10-03 20:56:18 -0300

    modificacion en swithcmodelo.js

--- swichtmodelo.js
+++ swichtmodelo.js
@@ -2,11 +2,11 @@ let dia = "viernes"
 
 switch(dia){ //switch siempre primero (la variable a verificar)
    case "viernes":
-   console.log("buen finde")
+   console.log("buen finde perro loco")
    break
    case "lunes": //case para comprobar la variable 
    console.log("buena semana")
    break //siempre despues de console.log dentro de case
    default:
-   console.log("buen dia")
+   console.log("la paz nunca fue una opcion")
 }
