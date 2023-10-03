0eb65373d98465ca4eac9b7b7ef09c0a10a13001
ramiro125 <ramiro12526@gmail.com>
2023-10-03 20:59:50 -0300

    Cambio en dia

--- swichtmodelo.js
+++ swichtmodelo.js
@@ -4,9 +4,12 @@ switch(dia){ //switch siempre primero (la variable a verificar)
    case "viernes":
    console.log("buen finde perro loco")
    break
+   case "jueves":
+   console.log("como estan")
+   break
    case "lunes": //case para comprobar la variable 
-   console.log("buena semana")
+   console.log("Buen juicio final")
    break //siempre despues de console.log dentro de case
    default:
-   console.log("la paz nunca fue una opcion")
+   console.log("la paz siempre fue una opcion")
 }
