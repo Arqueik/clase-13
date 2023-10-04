2baf15306b4200d7f44d2644108f2324d8cff771
Alan Quiroga <arqueikk@gmail.com>
2023-10-03 21:06:35 -0300

    last change

--- swichtmodelo.js
+++ swichtmodelo.js
@@ -2,7 +2,7 @@ let dia = "viernes"
 
 switch(dia){ //switch siempre primero (la variable a verificar)
    case "viernes":
-   console.log("buen finde perro loco"console)
+   console.log("buen finde perro loco")
    break
    case "jueves":
    console.log("como estan")
@@ -11,9 +11,6 @@ switch(dia){ //switch siempre primero (la variable a verificar)
    console.log("Buen juicio final")
    break //siempre despues de console.log dentro de case
    default:
-<<<<<<< HEAD
    console.log("la paz siempre fue una opcion")
-=======
-   console.log("la paz nunca fue una opcion")::
->>>>>>> d3272c0ff6555dece9fa849faaecf4af6d230bfa
 }
+
